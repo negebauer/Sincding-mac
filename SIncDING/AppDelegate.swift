3f1a16e8e6f794383447013b1b75c7047ef4d87f
negebauer <negebauer@uc.cl>
2016-03-08 18:23:15 -0300

    Initial Commit

---
+++ SIncDING/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SIncDING
+//
+//  Created by Nicolás Gebauer on 08-03-16.
+//  Copyright © 2016 Nicolás Gebauer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SIncDING/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SIncDING
+//
+//  Created by Nicolás Gebauer on 08-03-16.
+//  Copyright © 2016 Nicolás Gebauer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
