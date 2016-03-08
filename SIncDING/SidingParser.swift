28f4dab246ad978ccd0c16b5cf324e6cab6cac01
negebauer <negebauer@uc.cl>
2016-03-08 20:25:39 -0300

    Login, llegado a carpeta IngCursos

--- SIncDING/SidingParser.swift
+++ SIncDING/SidingParser.swift
@@ -7,3 +7,86 @@
 //
 
 import Foundation
+import Alamofire
+import Kanna
+
+class SidingParser {
+    
+    // MARK: - Constants
+    
+    let sidingLogin = "https://intrawww.ing.puc.cl/siding/index.phtml"
+    let sidingDomain = "intrawww.ing.puc.cl"
+    let sidingSite = "https://intrawww.ing.puc.cl/siding/dirdes/ingcursos/cursos/vista.phtml"
+
+    // MARK: - Variables
+    
+    var username: String
+    var password: String
+    var cookies: [NSHTTPCookie] = []
+    
+    // MARK: - Init
+    
+    init(username: String, password: String) {
+        self.username = username
+        self.password = password
+    }
+    
+    // MARK: - Functions
+
+    func headers() -> [String: String] {
+        return NSHTTPCookie.requestHeaderFieldsWithCookies(cookies)
+    }
+    
+    func doStuff() {
+        let params: [String: String] = [
+            "login": username,
+            "passwd": password,
+            "sw": "",
+            "sh": "",
+            "cd": ""
+        ]
+        Alamofire.request(.POST, sidingLogin, parameters: params,
+            encoding: .URL)
+            .response { (_, response, data, error) in
+                if error != nil {
+                    print("Error: \(error!)")
+                } else {
+                    let cookies = NSHTTPCookie.cookiesWithResponseHeaderFields(response!.allHeaderFields as! [String: String], forURL: NSURL(string: self.sidingDomain)!)
+                    self.cookies.appendContentsOf(cookies)
+                    self.doStuff2()
+                }
+        }
+    }
+    
+    func doStuff2() {
+        Alamofire.request(.GET, sidingSite, headers: headers())
+            .response { (_, response, data, error) in
+                if error != nil {
+                    print("Error: \(error!)")
+                } else {
+                    self.parseSiteHTML(String(data: data!, encoding: NSASCIIStringEncoding)!)
+                }
+        }
+    }
+    
+    func parseSiteHTML(data: String) {
+        if let doc = Kanna.HTML(html: data, encoding: NSUTF8StringEncoding) {
+            print(doc.title)
+            // Search for nodes by XPath
+            for link in doc.xpath("//a | //link") {
+                print(link.text)
+                print(link["href"])
+            }
+            
+            Alamofire.request(.GET, sidingSite + "?accion_curso=avisos&acc_aviso=mostrar&id_curso_ic=8036", headers: headers())
+                .response { (_, response, data, error) in
+                    if error != nil {
+                        print("Error: \(error!)")
+                    } else {
+                        print("Response: \(response)")
+                    }
+            }
+            
+        }
+    }
+}
--- SIncDING/ViewController.swift
+++ SIncDING/ViewController.swift
@@ -9,22 +9,58 @@
 import Cocoa
 import Crashlytics
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, NSTextFieldDelegate {
+    
+    // MARK: - Constants
+    
+    // MARK: - Variables
+    
+    var sidingParser: SidingParser!
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var usernameField: NSTextField!
+    @IBOutlet weak var passwordField: NSSecureTextField!
+    
+    // MARK: - Init
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-
+        usernameField.stringValue = "negebauer"
+        passwordField.stringValue = ""
     }
-
-
+    
+    
     override var representedObject: AnyObject? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
+    
+    // MARK: - Actions
+    
+    @IBAction func doStuff(sender: AnyObject) {
+        if sidingParser == nil {
+            guard usernameField.stringValue != "" && passwordField.stringValue != "" else {
+                let alert = NSAlert()
+                alert.addButtonWithTitle("Ok, sorry")
+                alert.messageText = "Pon tu usuario y tu clave po"
+                alert.informativeText = "Como queri que funcione sin eso..."
+                alert.alertStyle = .WarningAlertStyle
+                alert.runModal()
+                return
+            }
+            sidingParser = SidingParser(username: usernameField.stringValue, password: passwordField.stringValue)
+        }
+        sidingParser.doStuff()
+    }
+    
+    @IBAction func passwordEnterPressed(sender: AnyObject) {
+        doStuff(sender)
+    }
+    
+    // MARK: - Functions
+    
+    
 }
 
